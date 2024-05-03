ba062b2cfe91006f457a8990ecdd3ed8024b68cb
JMH2323 <johnmasonhaley23@gmail.com>
2024-05-02 23:50:47 -0400

    Created Game Class. Window, Render

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <SDL.h>
 #include <cstdio>
+#include "Game.hpp"
 
 
-int main(int argc, const char* argv[])
+Game* game = nullptr;
+
+int main(int argc, char* argv[])
 {
 
+	game = new Game();
 
+	game->init("RoguedSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
+	while (game->running())
+	{
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
 
-
-
-
+	game->clean();
 
 	return 0;
 }
